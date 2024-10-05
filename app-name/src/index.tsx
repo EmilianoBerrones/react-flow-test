9feb379fe4d6161ef7b282cd5a93b2045bfaf24f
Daniel Méndez Beltrán <mendezboy2003@hotmail.com>
2024-10-05 17:20:43 -0400

    test

--- app-name/src/index.tsx
+++ app-name/src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 //import App from './App';
-import Login from './Login.tsx'
+import Login from './Login'
 
 import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import {DialogProvider} from "./DialogContext.tsx";
+import {DialogProvider} from "./DialogContext";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
