f9aafcfdf50fa4ccfcfb6f67fe7fc4eb3ed10ba5
Daniel Méndez Beltrán <mendezboy2003@hotmail.com>
2024-10-07 16:11:17 -0400

    Blank landing page and blank graphs

--- app-name/src/edges/index.ts
+++ app-name/src/edges/index.ts
@@ -8,14 +8,7 @@ const arrowMarker = {
 }
 
 export const initialEdges = [
-    {id: 'G1 to C1', source: 'G1', target: 'C1', animated: true, type: 'step', markerEnd: arrowMarker, style: {color: 'red'}},
-    {id: 'G1 to C2', source: 'G1', target: 'C2', animated: true, type: 'step', markerEnd: arrowMarker, style: {color: 'red'}},
-    {id: 'G1 to G2', source: 'G1', target: 'G2', animated: true, type: 'step', markerEnd: arrowMarker, style: {color: 'red'}},
-    {id: 'G2 to C3', source: 'G2', target: 'C3', animated: true, type: 'step', markerEnd: arrowMarker, style: {color: 'red'}},
-    {id: 'G2 to S1', source: 'G2', target: 'S1', animated: true, type: 'step', markerEnd: arrowMarker, style: {color: 'red'}},
-    {id: 'S1 to A1', source: 'S1', target: 'A1', animated: true, type: 'step', markerEnd: arrowMarker, style: {color: 'red'}},
-    {id: 'S1 to G3', source: 'S1', target: 'G3', animated: true, type: 'step', markerEnd: arrowMarker, style: {color: 'red'}},
-    {id: 'G3 to Sn1', source: 'G3', target: 'Sn1', animated: true, type: 'step', markerEnd: arrowMarker, style: {color: 'red'}},
+
 ] satisfies Edge[];
 
 export const edgeTypes = {
